671cc78198d24ac993d44e252ef514c4493eaa19
samardheage-cmd <samardh.eage@gmail.com>
2026-03-04 10:34:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
